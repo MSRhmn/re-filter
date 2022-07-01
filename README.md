272b2f67b9ed8156277695da5c0fc85066228312
MS Rahman <apubdtea@gmail.com>
2022-07-01 16:24:18 +0600

    Added new filter for emails

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Filtering phone numbers from users data file using regular expression.
+# Filtering phone numbers and email addresses in separate file, from users data file using regular expression.
 
 -apart from validating data, `re` is very handy when you need to extract specific data from a large file.
--- re_filter.py
+++ re_filter.py
@@ -1,11 +1,18 @@
 import re
 
-pattern = re.compile(r'\d{3}[-.]\d{3}[-.]\d{4}')
+num_pattern = re.compile(r'\d{3}[-.]\d{3}[-.]\d{4}')
+email_pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
 
 with open('./data/data.txt', 'r') as rf:
   contents = rf.read()
   with open('./data/phone_numbers.txt', 'w') as wf:
-    matches = pattern.finditer(contents)
+    matches = num_pattern.finditer(contents)
+
+    for match in matches:
+      wf.write(match.group(0) + '\n')
+  
+  with open('./data/email_addresses.txt', 'w') as wf:
+    matches = email_pattern.finditer(contents)
 
     for match in matches:
       wf.write(match.group(0) + '\n')
